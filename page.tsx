c0abf742f4ba9d80183c79aa25539ed77958dda0
shifabaloch13 <167632110+shifabaloch13@users.noreply.github.com>
2024-10-10 05:03:27 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Hero from "./components/hero";    
+import Card from "./components/card";
+
+export default function home () {
+ return (
+  <main>
+
+    <Hero/>
+    <Card/>
+  </main>
+ )
+}
